be013dc115a3731b58f817edd75b524f2e39e7e7
Wenye <wenye_01@outlook.com>
2023-12-20 18:40:56 +0800

    update

--- src/render/PipelineStateObject.h
+++ src/render/PipelineStateObject.h
@@ -15,6 +15,16 @@ namespace Gloria
 
     };
 
+    struct PSOInfo
+    {
+        std::string InputLayoutName;
+        Shader* pShader = nullptr;
+        DXGI_FORMAT RTVFormats[8] = { DXGI_FORMAT_R8G8B8A8_UNORM };
+        bool _4xMsaaState = false;
+        UINT _4xMsaaQuality = 0;
+        DXGI_FORMAT DepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    };
+
     class GraphicsPSOManage
     {
     public:
@@ -24,6 +34,8 @@ namespace Gloria
 
         void TryCreatePSO();
 
+    private:
+        void CreatePSO();
     private:
         D3D12Common* pCommon = nullptr;
 
