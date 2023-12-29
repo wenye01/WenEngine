40366f4374fa4217bedb00de98b599326e55edec
Wenye <wenye_01@outlook.com>
2023-12-29 15:04:16 +0800

    update

--- src/render/PipelineStateObject.cpp
+++ src/render/PipelineStateObject.cpp
@@ -2,5 +2,22 @@
 
 namespace Gloria
 {
-    
+    GraphicsPSOManage::GraphicsPSOManage(D3D12Common* pcommon, InputLayout* inputLayout)
+        :pCommon(pcommon), pInputLayout(inputLayout)
+    {
+
+    }
+
+    GraphicsPSOManage::~GraphicsPSOManage()
+    {
+
+    }
+
+    void GraphicsPSOManage::TryCreatePSO()
+    {
+        D3D12_GRAPHICS_PIPELINE_STATE_DESC desc;
+        {
+
+        }
+    }
 }
--- src/render/PipelineStateObject.h
+++ src/render/PipelineStateObject.h
@@ -17,18 +17,32 @@ namespace Gloria
 
     struct PSOInfo
     {
+        bool operator==(const PSOInfo pso) const
+        {
+            return (pso.InputLayoutName == this->InputLayoutName
+                && pso.pShader == this->pShader
+                && pso.PrimitiveTopologyType == this->PrimitiveTopologyType
+                && pso.RasterizerDesc.CullMode == this->RasterizerDesc.CullMode
+                && pso.DepthStencilDesc.DepthFunc == this->DepthStencilDesc.DepthFunc);
+        }
+
         std::string InputLayoutName;
         Shader* pShader = nullptr;
         DXGI_FORMAT RTVFormats[8] = { DXGI_FORMAT_R8G8B8A8_UNORM };
         bool _4xMsaaState = false;
         UINT _4xMsaaQuality = 0;
         DXGI_FORMAT DepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+        D3D12_RASTERIZER_DESC RasterizerDesc = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+        D3D12_BLEND_DESC BlendDesc = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+        D3D12_DEPTH_STENCIL_DESC DepthStencilDesc = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
+        D3D12_PRIMITIVE_TOPOLOGY_TYPE PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+        UINT NumRenderTargets = 1;
     };
 
     class GraphicsPSOManage
     {
     public:
-        GraphicsPSOManage();
+        GraphicsPSOManage(D3D12Common* pcommon, InputLayout* inputLayout);
 
         ~GraphicsPSOManage();
 
@@ -39,6 +53,17 @@ namespace Gloria
     private:
         D3D12Common* pCommon = nullptr;
 
-        InputLayout* inputLayout = nullptr;
+        InputLayout* pInputLayout = nullptr;
+
+        std::unordered_map<PSOInfo, Microsoft::WRL::ComPtr<ID3D12PipelineState>> PSOMap;
+    };
+}
+
+namespace std
+{
+    template <>
+    struct hash<Gloria::PSOInfo>
+    {
+        //TODO
     };
 }
